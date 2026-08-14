ad64671a722ee330a42be8e1b635005c758e0d0a
Ishaansharma01 <ishaansharma103590@gmail.com>
2026-08-14 10:21:55 +0530

    Lab1: 6 programs

---
+++ Lab 1/celsius_to_fahrenheit.py
@@ -0,0 +1,5 @@
+#Celsius to Fahrenheit conversion
+cel=float(input("Enter temprature in Celsius: "))
+fah=float((cel * (9/5)) + 32)
+print("Temp in Celsius: ",cel)
+print("Temp in Fahrenheit", fah)
---
+++ Lab 1/escape_sequence.py
@@ -0,0 +1,9 @@
+#Small food bill Reciept program
+print("--------------Food Bill---------------")
+print("Food Item\tQuantity\tPrice")
+print("Rolls\t\t3\t\t150")
+print("Burger\t\t2\t\t100")
+print("Coffee\t\t2\t\t250")
+print("--------------------------------------")
+print("Total:\t\t\t\t500")
+print("--------------------------------------")
